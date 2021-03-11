8d5529eeba674f597ea391a8a0b147cd18a30237
Наиль Шаряпов <zolman99@gmail.com>
2021-03-11 20:18:34 +0300

    Started v1

--- starter.py
+++ starter.py
@@ -3,9 +3,9 @@ import Server.Server
 import ServiceToServer.Service
 import ServiceHardware.Worker
 if __name__ == '__main__':
-    thread1 = threading.Thread(target=Server.Server.exec())
-    thread2 = threading.Thread(target=ServiceToServer.Service.exec())
-    thread3 = threading.Thread(target=ServiceHardware.Worker.exec())
+    thread1 = threading.Thread(target=Server.Server.exec)
+    thread2 = threading.Thread(target=ServiceToServer.Service.exec)
+    thread3 = threading.Thread(target=ServiceHardware.Worker.exec)
     thread1.start()
     thread2.start()
     thread3.start()
